a3c553c9438b9443b69bbbe270ca4d937051e25e
Michael Friedrich <michael.friedrich@netways.de>
2019-01-19 17:03:02 +0100

    Refactor into dedicated build script

---
+++ build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+git clone https://github.com/icinga/icinga2.git
+cd icinga2
+
+mkdir release
+cd release
+# Unity builds off, we're in Docker
+cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/tmp/icinga2 -DICINGA2_PLUGINDIR=/tmp/icinga2/sbin -DICINGA2_UNITY_BUILD=OFF
+cd ..
+make -j 2 -C release
+make test
+make install
+/tmp/icinga2/sbin/icinga2 --version
+/tmp/icinga2/sbin/icinga2 daemon -C -DRunAsUser=$(id -u -n) -DRunAsGroup=$(id -g -n)
+
--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,22 +2,3 @@
 
 sh -c "echo $*"
 
-apt-get -y --no-install-recommends install \
-  git cmake make ccache build-essential \
-  libboost-all-dev flex bison libssl-dev libpq-dev libmysqlclient-dev \
-  libedit-dev libyajl-dev
-
-git clone https://github.com/icinga/icinga2.git
-cd icinga2
-
-mkdir release
-cd release
-# Unity builds off, we're in Docker
-cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/tmp/icinga2 -DICINGA2_PLUGINDIR=/tmp/icinga2/sbin -DICINGA2_UNITY_BUILD=OFF
-cd ..
-make -j 2 -C release
-make test
-make install
-/tmp/icinga2/sbin/icinga2 --version
-/tmp/icinga2/sbin/icinga2 daemon -C -DRunAsUser=$(id -u -n) -DRunAsGroup=$(id -g -n)
-
