dea650e55eec624101f49e111c9435ba3dbcd6a7
Michael Friedrich <michael.friedrich@netways.de>
2019-01-19 17:12:40 +0100

    Fix cmake build

--- build/build.sh
+++ build/build.sh
@@ -3,10 +3,10 @@
 git clone https://github.com/icinga/icinga2.git
 cd icinga2
 
-mkdir release
+mkdir -p release
 cd release
 # Unity builds off, we're in Docker
-cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/tmp/icinga2 -DICINGA2_PLUGINDIR=/tmp/icinga2/sbin -DICINGA2_UNITY_BUILD=OFF
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/tmp/icinga2 -DICINGA2_PLUGINDIR=/tmp/icinga2/sbin -DICINGA2_UNITY_BUILD=OFF ..
 cd ..
 make -j 2 -C release
 make test
