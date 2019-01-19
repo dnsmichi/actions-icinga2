99ea63f386789240161fb061cdc141959a8ad62d
Michael Friedrich <michael.friedrich@netways.de>
2019-01-19 17:22:13 +0100

    Some more logging

--- build/build.sh
+++ build/build.sh
@@ -3,11 +3,17 @@
 git clone https://github.com/icinga/icinga2.git
 cd icinga2
 
+echo "Building Icinga 2 in $(pwd)\n"
+
 mkdir -p release
 cd release
+
+echo "Preparing CMake in $(pwd)\n"
 # Unity builds off, we're in Docker
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/tmp/icinga2 -DICINGA2_PLUGINDIR=/tmp/icinga2/sbin -DICINGA2_UNITY_BUILD=OFF ..
 cd ..
+
+echo "Running make in $(pwd)\n"
 make -j 2 -C release
 make test
 make install
